6c29d49247f9e1867c5af8e5ff23430a5d0e1f93
mmg <mustafamhmoud212003@gmail.com>
2023-05-11 21:49:55 +0300

    iphone_disgin

---
+++ main.js
@@ -0,0 +1,14 @@
+let img = document.querySelector(".imgess");
+let container = document.querySelector(".container");
+
+function phones(phone) {
+
+  img.src = phone;
+
+}
+
+function background_color(colors) {
+
+  container.style.background = colors;
+
+}
